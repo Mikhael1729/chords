a81508661c61da7417bada718126f34749d17a29
Mikhael <msantos@truora.com>
2022-01-15 21:49:00 -0400

    Add test caes for maj and min

---
+++ chords_translator_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestChordsTranslator_Process(t *testing.T) {
+	assert := require.New(t)
+
+	testCases := []struct {
+		word               []string
+		expectedChordNotes string
+	}{
+		{[]string{"B", "min"}, "B, D, F#"},
+		{[]string{"B#"}, "B#, D#, F#"},
+	}
+
+	translator := NewChordsTranslator()
+
+  for _, testCase := range testCases {
+    notes := translator.Process(testCase.word)
+    assert.Equal(testCase.expectedChordNotes, notes)
+  }
+}
